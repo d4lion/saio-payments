c49f45aecb2660be12d7206f1d673a86fd993685
d4lion <d4lion.dev@gmail.com>
2026-08-11 01:09:07 -0500

    feat: Refactor Transaction class to use Lombok for getters and add new fields

--- src/main/java/cloud/adamind/saio/payments/dto/Transaction.java
+++ src/main/java/cloud/adamind/saio/payments/dto/Transaction.java
@@ -1,7 +1,10 @@
 package cloud.adamind.saio.payments.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Transaction {
     private String id;
@@ -16,6 +19,10 @@ public class Transaction {
 
     private String paymentMethodType;
 
+    private PaymentMethod paymentMethod;
+
+    private CustomerData customerData;
+
     private String redirectUrl;
 
     private String status;
@@ -25,92 +32,4 @@ public class Transaction {
     private Object paymentLinkId;
 
     private Object paymentSourceId;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public Integer getAmountInCents() {
-        return amountInCents;
-    }
-
-    public void setAmountInCents(Integer amountInCents) {
-        this.amountInCents = amountInCents;
-    }
-
-    public String getReference() {
-        return reference;
-    }
-
-    public void setReference(String reference) {
-        this.reference = reference;
-    }
-
-    public String getCustomerEmail() {
-        return customerEmail;
-    }
-
-    public void setCustomerEmail(String customerEmail) {
-        this.customerEmail = customerEmail;
-    }
-
-    public String getCurrency() {
-        return currency;
-    }
-
-    public void setCurrency(String currency) {
-        this.currency = currency;
-    }
-
-    public String getPaymentMethodType() {
-        return paymentMethodType;
-    }
-
-    public void setPaymentMethodType(String paymentMethodType) {
-        this.paymentMethodType = paymentMethodType;
-    }
-
-    public String getRedirectUrl() {
-        return redirectUrl;
-    }
-
-    public void setRedirectUrl(String redirectUrl) {
-        this.redirectUrl = redirectUrl;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public Object getShippingAddress() {
-        return shippingAddress;
-    }
-
-    public void setShippingAddress(Object shippingAddress) {
-        this.shippingAddress = shippingAddress;
-    }
-
-    public Object getPaymentLinkId() {
-        return paymentLinkId;
-    }
-
-    public void setPaymentLinkId(Object paymentLinkId) {
-        this.paymentLinkId = paymentLinkId;
-    }
-
-    public Object getPaymentSourceId() {
-        return paymentSourceId;
-    }
-
-    public void setPaymentSourceId(Object paymentSourceId) {
-        this.paymentSourceId = paymentSourceId;
-    }
 }
